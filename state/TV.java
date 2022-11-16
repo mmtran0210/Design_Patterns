c35f97d65c986779a127fc7e3d052950c8309164
mmtran0210 <mi.minh.tran@gmail.com>
2022-11-16 16:37:57 -0500

    class sub

--- state/TV.java
+++ state/TV.java
@@ -1,5 +1,29 @@
 package state;
 
 public class TV {
-    
+    private State HomeState;
+    private State NetflixState;
+    private State HuluState;
+    private State state;
+
+
+public TV(){}
+
+
+
+public State getNetflixState(){
+    return NetflixState;
+}
+
+public State getHomeState(){
+    return HomeState;
+}
+
+public State getHuluState(){
+    return HuluState;
+}
+
+public void setState(State state){
+    this.state = state;
+}
 }
