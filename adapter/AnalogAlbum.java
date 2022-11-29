f70eab370750cfe9a4f502d08fa96192cf6557c6
mmtran0210 <mi.minh.tran@gmail.com>
2022-11-29 00:25:01 -0500

    adapter

---
+++ adapter/AnalogAlbum.java
@@ -0,0 +1,9 @@
+package adapter;
+
+public interface AnalogAlbum {
+    public String play();
+    public String rewinid();
+    public String ffwd();
+    public String pause();
+    public String stopEject();
+}
